5650311246f16182bf3e4fbdafe99e3bfff71acb
codeTukai <tukaighosh753@gmail.com>
2026-05-22 22:36:26 +0530

    commit

--- Node/index.js
+++ Node/index.js
@@ -1,5 +1,13 @@
-function add(a,b){
-return a+b
+const thenable ={
+    then: function(onFulfilled){
+        setTimeout(() =>onFulfilled(42), 10);
+    }
 }
 
-console.log(add(8,9));
+async function main() {
+    const v = await thenable;
+    console.log("Hello world");
+    
+}
+
+main()
