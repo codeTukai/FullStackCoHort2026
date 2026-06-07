f1a77f72af44d863e78992e0752f936696327c32
codeTukai <tukaighosh753@gmail.com>
2026-06-07 19:59:56 +0530

    Refactor Express server to include request count middleware and update endpoint responses

--- singleMiddleware/loginBackend.js
+++ singleMiddleware/loginBackend.js
@@ -1,7 +1,28 @@
 import express from 'express'
 const app = express()
 
-app.get('', function(req,res){
-    res.send("Hello World")
+let reqCount = 0;
+
+app.use(function(req,res,next){
+    reqCount = reqCount + 1
+    next();
 })
-app.listen(3001)
+
+app.get('/', function(req,res){
+    res.status(200).json(
+        {
+            name: "john"
+        }
+    );
+});
+app.get('/req', function(req,res){
+    res.status(200).json(
+        {
+            reqCount
+        }
+    );
+});
+app.listen(3000,()=>{
+    console.log("port running on 3000");
+    
+})
