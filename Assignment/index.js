bc08afde1709f8ca00e52efab73539f14093abf8
codeTukai <tukaighosh753@gmail.com>
2026-06-27 18:48:25 +0530

    Add package.json for project setup with Express dependency

--- Assignment/index.js
+++ Assignment/index.js
@@ -4,37 +4,17 @@ const app = express()
 
 app.get("/sum", function(req, res){
 
- const a = req.query.a;
- const b = req.query.b
+ const a = parseInt(req.query.a);
+ const b = parseInt(req.query.b);
 
     res.json({
-        ans: a+b,
+        answer: a + b,
         
     })
 
-    res.send("hii express", sum)
+    res.send("hii express", answer)
 })
-app.get("/multiply", function(req, res){
 
-    const a = req.query.a;
-    const b = req.query.b;
-
-    res.json({
-        
-        ans: a * b,
-        
-    })
-})
-app.get("/subtract", function(req, res){
-
-
-    res.send("hii express")
-})
-app.get("/division", function(req, res){
-
-
-    res.send("hii express")
-})
 
 app.listen(3000, ()=>{
     console.log("App running on server 3000");
