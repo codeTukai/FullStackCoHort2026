6c3e1627ad2045a9f3655e5ada48c10b0f0f50f9
codeTukai <tukaighosh753@gmail.com>
2026-06-27 18:25:52 +0530

    Implement arithmetic endpoints for sum and multiply operations

--- Assignment/index.js
+++ Assignment/index.js
@@ -2,10 +2,37 @@ import express from 'express'
 
 const app = express()
 
-app.get("/", function(req, res){
-    let a;
-    let b;
-    
+app.get("/sum", function(req, res){
+
+ const a = req.query.a;
+ const b = req.query.b
+
+    res.json({
+        ans: a+b,
+        
+    })
+
+    res.send("hii express", sum)
+})
+app.get("/multiply", function(req, res){
+
+    const a = req.query.a;
+    const b = req.query.b;
+
+    res.json({
+        
+        ans: a * b,
+        
+    })
+})
+app.get("/subtract", function(req, res){
+
+
+    res.send("hii express")
+})
+app.get("/division", function(req, res){
+
+
     res.send("hii express")
 })
 
